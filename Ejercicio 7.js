cc498300ba796d3399bbeb29e4cb6d09f0f828b4
jatorrey <jacatorresre@ittepic.edu.mx>
2024-08-28 21:23:13 -0700

    first commit

---
+++ Ejercicio 7.js
@@ -0,0 +1,9 @@
+// 7. Concatenación de arreglos
+// Torres Reyes Jaime Camil
+function concatenarArreglos(arreglo1, arreglo2) {
+    return arreglo1.concat(arreglo2);
+}
+
+const arreglo1 = ["Hola", "Mundo"];
+const arreglo2 = ["!", "¿Cómo", "estás?"];
+console.log("Arreglo concatenado:", concatenarArreglos(arreglo1, arreglo2));
